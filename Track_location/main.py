973a4a8691446310e0ac8f484d7068d0e6e89379
Rudranil Goswami <nilrudra068@gmail.com>
2025-08-07 18:30:56 +0530

    Initial commit

---
+++ Track_location/main.py
@@ -0,0 +1,24 @@
+import phonenumbers
+import opencage
+import folium
+from myphone import number
+
+from phonenumbers import geocoder
+pepnumber = phonenumbers.parse(number);
+location = geocoder.description_for_number(pepnumber,"en")
+print(location)
+
+from phonenumbers import carrier
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider, "en"))
+from opencage.geocoder import OpenCageGeocode
+key="f10c82a32b614ebabf0ef6973bcf1a53"
+geocoder = OpenCageGeocode(key)
+query = str(location)
+results = geocoder.geocode(query)
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+print(lat,lng)
+myMap = folium.Map(location=[lat,lng],zoom_start= 9 )
+folium.Marker([lat,lng],popup=location).add_to(myMap)
+myMap.save("Mylocation.html")
